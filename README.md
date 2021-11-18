6185cf27246ae2eca5bef8b461d033fd266d9439
Kennedy Dorsey <kadorsey97@gmail.com>
2021-11-17 19:30:07 -0600

    Add image and information to give context to simulation being ran. Changed variable name from sample to trial to be less ambiguous. Updated README to reflect changes.

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Visualzing Central Limit Theorem
+# Visualizing the Central Limit Theorem for Sums
 
-This is a flask app that helps to visualize the central limit theorem. Using the box model, summing up a number of draws taken randomly with replacement will eventually follow the normal distribution regardless of the makeup of the box as the number of draws taken increases.
+This is a website made using flask that helps to visualize the central limit theorem when applied to sums. Using the box model, summing up a number of draws taken randomly with replacement from a box with specified "tickets" will eventually follow the normal distribution regardless of the contents of the box as the number of draws taken increases.
 
 The expected value of the sum of the draws is the average of the box multiplied by the number of draws.
-The standard error of the sum of the draws is the standard deviation of the box multipied by the square root of the number of draws.
+The standard error of the sum of the draws is the standard deviation of the box multiplied by the square root of the number of draws. This can be seen for larger numbers of draws.
 
-This app allows you to choose the number of draws you want to take and the number of trials you want to do. As the number of draws increases, and at a good trial size, the data begins to closely resemble the normal probability distribution.
+This flask app allows you to choose the number of draws you want to take and the number of trials you want to do. As the number of draws increases, and at a good trial size, the data begins to closely resemble the normal probability distribution.
 
-The visualization was created using Chart.js. 
+The visualization was created using Chart.js.
 The app requirements are listed in requirements.txt.
-
-
--- clt_main.py
+++ clt_main.py
@@ -1,50 +1,51 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for
 import json
 import os
 import random
 
-bp = Blueprint('clt_main', __name__, url_prefix="/")
+bp = Blueprint("clt_main", __name__, url_prefix="/")
 
 
-@bp.route('/', methods=('GET','POST'))
+@bp.route("/", methods=("GET","POST"))
 def index():
    
     data = {}
 
-    if request.method == 'POST':
+    if request.method == "POST":
         num_draws = request.form["draws"]
-        num_samples = request.form["samples"]
+        num_trials = request.form["trials"]
 
-        # get sum data from drawing one ticket a given amount of times repeated for a given amount of times
-        for i in range(int(num_samples)):
-            sum = 0
+        # get sum data from drawing from a given range at random with replacement a given amount of times 
+        # repeated for a given amount of trials
+        for i in range(int(num_trials)):
+            sum_draws = 0
             for j in range(int(num_draws)):
-                draw = random.randint(1,5)
-                sum += draw
+                draw = random.randint(1, 6)
+                sum_draws += draw
 
             # get a count for number of occurrences of a particular sum
-            if sum in list(data):
-                data[sum] = data[sum] + 1
+            if sum_draws in list(data):
+                data[sum_draws] = data[sum_draws] + 1
             else:
-                data[sum] = 1
+                data[sum_draws] = 1
         
         # convert dictionary keys and values to two lists to convert to JSON 
-        # first sort by key value
+        # first sort by key value since to visualize data in ascending order by sum number
         sorted_count_data = []
         for key in sorted(data):
             sorted_count_data.append(data[key])
 
         data_to_json = {}
-        data_to_json['sum'] = list(sorted(data))
-        data_to_json['count'] = sorted_count_data
+        data_to_json["sum"] = list(sorted(data))
+        data_to_json["count"] = sorted_count_data
 
         # convert to JSON and write to a file
-        with open(os.path.join('clt/static','data.json'),'w') as outfile:
+        with open(os.path.join("clt/static/data.json"),"w") as outfile:
             json.dump(data_to_json, outfile)
         
-        return render_template('clt_main/index.html', draws=num_draws, samples=num_samples)
+        return render_template("clt_main/index.html", draws=num_draws, trials=num_trials)
 
-    num_draws = 'Not Given'
-    num_samples = 'Not Given'
+    num_draws = "Not Given"
+    num_trials = "Not Given"
 
-    return render_template('clt_main/index.html', draws=num_draws, samples=num_samples)
+    return render_template("clt_main/index.html", draws=num_draws, trials=num_trials)
